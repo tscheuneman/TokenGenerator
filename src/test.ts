ae7e9a5f721096006c474266c1695fe885dd86b8
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-02-17 01:21:47 -0700

    Adds some initial test cases

---
+++ src/test.ts
@@ -0,0 +1,24 @@
+import TokenGenerator from '.';
+
+describe('Tests for inital classes', () => {
+
+    const generator = new TokenGenerator('secret');
+
+    it('Should Encode a token', () => {
+        const token = generator.sign('test', 'data');
+        expect(token).toBeTruthy();
+    });
+
+    it('Should Encode and Decode a token', () => {
+        const token = generator.sign('test', 'data');
+        const decoded = generator.verify(token, 'test');
+
+        expect(decoded).toBe('data');
+    });
+
+    it('Should Encode and fail to decode with an inproper identity', () => {
+        const token = generator.sign('test', 'data');
+
+        expect(() => generator.verify(token, 'test2')).toThrow();
+    });
+});
