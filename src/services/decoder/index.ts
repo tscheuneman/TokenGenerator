f0a4e644f5ef55295ec429a360863689f2a8d948
Thomas Scheuneman <tscheuneman@users.noreply.github.com>
2022-02-17 01:14:13 -0700

    Add const for number of bytes in our dateTime

--- src/services/decoder/index.ts
+++ src/services/decoder/index.ts
@@ -2,16 +2,17 @@ import { arrayBufferToString }  from '../../utils/string';
 import { arrayBuffertoNumber } from '../../utils/number';
 import { DecodeTimeIdentityResults } from './types';
 
+import { DATE_BYTES } from './constants';
 
 export const decodeTimeIdentity = (token: string): DecodeTimeIdentityResults => {
 
     const tokenBuffer = Buffer.from(token, 'base64');
 
-    const decodedDate = new Uint8Array(8);
-    decodedDate.set(tokenBuffer.slice(0, 7));
+    const decodedDate = new Uint8Array(DATE_BYTES);
+    decodedDate.set(tokenBuffer.slice(0, DATE_BYTES-1));
 
-    const decodedUuid = new Uint8Array(tokenBuffer.byteLength - 8);
-    decodedUuid.set(tokenBuffer.slice(8));
+    const decodedUuid = new Uint8Array(tokenBuffer.byteLength - DATE_BYTES);
+    decodedUuid.set(tokenBuffer.slice(DATE_BYTES));
 
     return {
         datetime: arrayBuffertoNumber(decodedDate.buffer),
